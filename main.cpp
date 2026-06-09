039d6537105adef9ee0606dbed51c9c9f58457ff
tauntpeelenjoyer89 <h3llor4nothe3re@gmail.com>
2026-06-09 16:58:28 -0300

    main criado

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <oistream> 
+#include <string>
+#include <heroi.h>
+#include <inimigo.h>
+
+using namspace std;
+
+int main () {
+	
+	cout << exibir();
+	cout << mostrar();
+	
+	return 0;
+	
+}
+	
