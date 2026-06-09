76e102eda94b45c308024868cdcc81823eac2aca
tauntpeelenjoyer89 <h3llor4nothe3re@gmail.com>
2026-06-09 16:57:40 -0300

    personagem adicionado

---
+++ heroi.h
@@ -0,0 +1,16 @@
+#include <oistream> 
+#include <string>
+
+using namspace std;
+
+int main () {
+	
+void exibir (string nome, int vida, int nivel);
+cout << "--- " << nome << " ---" << endl;
+cout << vida << endl;
+cout << nivel << enl;
+
+return 0;
+	
+}
+
