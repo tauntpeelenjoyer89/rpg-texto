c6a815c91496bd5ba6ccc465709572a1dffacaae
tauntpeelenjoyer89 <h3llor4nothe3re@gmail.com>
2026-06-09 16:58:02 -0300

    inimigo adicionado

---
+++ inimigo.h
@@ -0,0 +1,16 @@
+#include <oistream> 
+#include <string>
+
+using namspace std;
+
+int main () {
+	
+void mostrar (string nome, int vida, string tipo);
+cout << "--- " << nome << " ---" << endl;
+cout << vida << endl;
+cout << tipo << enl;
+
+return 0;
+	
+}
+
